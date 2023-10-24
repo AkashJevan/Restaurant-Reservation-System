f7f5b0f49da08371e761bf7832fce9dc3f696021
AkashJevan <128166843+AkashJevan@users.noreply.github.com>
2023-10-23 22:16:15 -0400

    Update Customer.py

--- Customer.py
+++ Customer.py
@@ -1,10 +1,1 @@
-#Setting Customer info
-class Customer:
-    #Initializing customer info
-    def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
 
-     #Make into string
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
