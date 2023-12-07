eb3af3e293464142f01ceeca8027b282ce9983f4
adityasonawan <148799184+adityasonawan@users.noreply.github.com>
2023-12-07 12:57:22 -0500

    Update reservation_manager.py

--- reservation_manager.py
+++ reservation_manager.py
@@ -1,3 +1,6 @@
+import csv
+from reservation import Reservation
+
 class ReservationManager:
     def __init__(self):
         self.reservations = []
@@ -6,8 +9,25 @@ class ReservationManager:
         self.reservations.append(reservation)
 
     def save_to_file(self, file_path):
-        with open(file_path, 'w') as file:
-            file.write("Name\tDate\tTime\tParty Size\n")
+        with open(file_path, 'a', newline='') as file:
+            writer = csv.writer(file)
             for reservation in self.reservations:
-                file.write(f"{reservation.customer_name}\t{reservation.reservation_date}\t"
-                           f"{reservation.reservation_time}\t{reservation.party_size}\n")
+                writer.writerow(reservation.to_list())
+                
+    def load_from_file(self, file_path):
+        self.reservations = []  # Clear existing reservations
+        try:
+            with open(file_path, 'r') as file:
+                reader = csv.reader(file, delimiter=',')
+                header = next(reader)  # Skip the header row
+                for row in reader:
+                    # Assuming the row contains [name, date, time, party_size]
+                    customer_name, reservation_date, reservation_time, party_size = map(str.strip, row)
+                    reservation_obj = Reservation(customer_name, reservation_date, reservation_time, int(party_size))
+                    self.add_reservation(reservation_obj)
+                return [str(reservation) for reservation in self.reservations]  # Ensure correct data is returned
+
+        except FileNotFoundError:
+            # Handle the case where the file does not exist
+            pass
+        return []
