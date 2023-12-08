52012fb8c2241fd18000a33e5c1d15009a73f109
AkashJevan <128166843+AkashJevan@users.noreply.github.com>
2023-12-07 20:25:32 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -53,13 +53,14 @@ class ReservationGUI:
 
         if self.validate_date(date) and self.validate_time(time) and name and party_size.isdigit():
             reservation = Reservation(name, date, time, int(party_size))
-            self.reservation_manager.add_reservation(reservation)
-            self.reservation_manager.save_to_file('reservation.csv')
+            # Update the line below to use add_or_update_reservation
+            self.reservation_manager.add_or_update_reservation(reservation)
             messagebox.showinfo("Reservation Submitted", "Reservation has been successfully submitted!")
             self.clear_entries()
             self.load_reservations()
         else:
             messagebox.showerror("Error", "Please enter valid reservation details.")
+
 
     def validate_date(self, date_text):
         try:
@@ -82,26 +83,23 @@ class ReservationGUI:
         self.entry_party_size.delete(0, tk.END)
 
     def load_reservations(self):
-        reservations = self.reservation_manager.load_from_file('reservation.csv')
-        self.reservation_dropdown['values'] = reservations
+        self.reservation_manager.load_from_file('reservation.csv')
+        reservation_names = [r.customer_name for r in self.reservation_manager.reservations]
+        self.reservation_dropdown['values'] = reservation_names
 
-    def show_selected_reservation(self):
-        selected_reservation = self.reservation_var.get()
+    def show_selected_reservation(self, event=None):
+        selected_name = self.reservation_var.get()
 
-        reservation_obj = None
         for reservation in self.reservation_manager.reservations:
-            if reservation.customer_name == selected_reservation:
-                reservation_obj = reservation
-                break
+            if reservation.customer_name == selected_name:
+                details = f"Name: {reservation.customer_name}\n" \
+                        f"Date: {reservation.reservation_date}\n" \
+                        f"Time: {reservation.reservation_time}\n" \
+                        f"Party Size: {reservation.party_size}"
+                messagebox.showinfo("Reservation Details", details)
+                return  # Exit the loop once the match is found
 
-        if reservation_obj:
-            details = f"Name: {reservation_obj.customer_name}\n" \
-                      f"Date: {reservation_obj.reservation_date}\n" \
-                      f"Time: {reservation_obj.reservation_time}\n" \
-                      f"Party Size: {reservation_obj.party_size}"
-            messagebox.showinfo("Reservation Details", details)
-        else:
-            messagebox.showerror("Error", "Selected reservation not found!")
+        messagebox.showerror("Error", "Selected reservation not found!")
 
 if __name__ == "__main__":
     root = tk.Tk()
