07a1d3d6d5b24d555356d61c30f2e88a4d0efe70
AkashJevan <128166843+AkashJevan@users.noreply.github.com>
2023-12-07 20:26:09 -0500

    Update reservation_manager.py

--- reservation_manager.py
+++ reservation_manager.py
@@ -4,22 +4,33 @@ from reservation import Reservation
 class ReservationManager:
     def __init__(self):
         self.reservations = []
-        self.file_path = 'reservation.csv'  # Consistent file name
+        self.file_path = 'reservation.csv'
         self.load_from_file(self.file_path)
 
-    def add_reservation(self, reservation):
-        if not any(r.customer_name == reservation.customer_name and r.reservation_date == reservation.reservation_date for r in self.reservations):
-            self.reservations.append(reservation)
-            self.save_to_file(self.file_path)
+    def add_or_update_reservation(self, new_reservation):
+        index_to_update = None
+        for i, reservation in enumerate(self.reservations):
+            if reservation.customer_name == new_reservation.customer_name and reservation.reservation_date == new_reservation.reservation_date:
+                index_to_update = i
+                break
+
+        if index_to_update is not None:
+            print("Updating existing reservation")
+            self.reservations[index_to_update] = new_reservation
+        else:
+            print("Adding new reservation")
+            self.reservations.append(new_reservation)
+
+        self.save_to_file(self.file_path)
+
 
     def save_to_file(self, file_path):
         try:
-            with open(file_path, 'w', newline='') as file:  # Overwrite mode
+            with open(file_path, 'w', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(['Name', 'Date', 'Time', 'Party Size'])
                 for reservation in self.reservations:
                     writer.writerow(reservation.to_list())
-            print("File written successfully")
         except Exception as e:
             print(f"Error writing to file: {e}")
 
@@ -31,8 +42,8 @@ class ReservationManager:
                 next(reader)  # Skip the header
                 for row in reader:
                     if len(row) == 4:
-                        row[3] = int(row[3])  # Convert party size to integer
+                        row[3] = int(row[3])
                         reservation_obj = Reservation(*row)
                         self.reservations.append(reservation_obj)
         except FileNotFoundError:
-            print(f"No existing file found at {file_path}.")
+            print(f"No existing file found at {file_path}, starting fresh.")
