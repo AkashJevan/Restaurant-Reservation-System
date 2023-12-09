8e21156eb8f42a05dae41cd1e4fa72203da0e90d
AkashJevan <128166843+AkashJevan@users.noreply.github.com>
2023-12-08 20:47:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import tkinter as tk
-from gui import ReservationGUI
+import tkinter as tk #import tkinter for gui
+from gui import ReservationGUI #import class from gui file
 
+#initialize & run reservation GUI
 def main():
     root = tk.Tk()
     app = ReservationGUI(root)
     root.mainloop()
 
+#only run when file is main
 if __name__ == "__main__":
     main()
